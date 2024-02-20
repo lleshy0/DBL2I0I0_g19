621f5c3529a0afd73f3c694e6ebcaaf08ae3d432
efsaneyildiz <131857189+efsaneyildiz@users.noreply.github.com>
2024-02-20 11:46:11 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import pm4py
+
+def import_xes(file_path):
+    event_log = pm4py.read_xes(file_path)
+    event_df = pm4py.convert_to_dataframe(event_log)
+    print(event_df)
+    start_activities = pm4py.get_start_activities(event_log)
+    end_activities = pm4py.get_end_activities(event_log)
+    print("Start activities: {}\nEnd activities: {}".format(start_activities, end_activities))
+
+if __name__ == "__main__":
+    file_path = r"C:\Users\20221343\Downloads\BPI Challenge 2012_1_all\BPI_Challenge_2012.xes.gz"
+    import_xes(file_path)

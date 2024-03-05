8bb5409d03a34a63697b56c270beacbbc00bd1b2
Maciej Bober <maciekbober03@gmail.com>
2024-03-05 11:07:39 +0100

    Fixed data cleaning.

--- clean.py
+++ clean.py
@@ -2,6 +2,7 @@ import pm4py
 from sklearn.model_selection import train_test_split
 import pandas as pd
 from sklearn.model_selection import train_test_split
+import numpy as np
 
 def xes_to_df(file_path):
     event_log = pm4py.read_xes(file_path)
@@ -10,14 +11,16 @@ def xes_to_df(file_path):
     return event_df
 
 if __name__ == "__main__":
-    file_path = r"C:\Users\20191663\Documents\Y5\Y5Q3\2IOI0 - DBL process mining\BPI Challenge 2012_1_all\BPI_Challenge_2012.xes\BPI_Challenge_2012.xes"
+    file_path = r""
     event_df = xes_to_df(file_path)
     cleaned_df = event_df.drop_duplicates()
 
-    train_traces, test_traces = pm4py.split_train_test(cleaned_df, train_percentage=0.75, case_id_key='case:concept:name')
+    train_traces, test_traces= np.split(cleaned_df, [int(.75 *len(cleaned_df))])
 
     sorted_test = test_traces.sort_values(by='case:REG_DATE')
 
     lowest_start_time = sorted_test['case:REG_DATE'].iloc[0]
 
     train_traces = train_traces[train_traces['time:timestamp'] <= lowest_start_time]
+
+    print(train_traces)

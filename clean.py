46a4f0282a62683680a059a2bf9de5769d11c4a5
Maciej Bober <maciekbober03@gmail.com>
2024-03-12 11:42:01 +0100

    Improved split.

--- clean.py
+++ clean.py
@@ -14,13 +14,20 @@ if __name__ == "__main__":
     file_path = r""
     event_df = xes_to_df(file_path)
     cleaned_df = event_df.drop_duplicates()
+    sorted_df = cleaned_df.sort_values(by='case:REG_DATE')
 
-    train_traces, test_traces= np.split(cleaned_df, [int(.75 *len(cleaned_df))])
+    train_traces, test_traces= np.split(sorted_df, [int(.75 *len(sorted_df))])
 
     sorted_test = test_traces.sort_values(by='case:REG_DATE')
+    
+    sorted_train  = train_traces.sort_values(by='case:REG_DATE')
 
     lowest_start_time = sorted_test['case:REG_DATE'].iloc[0]
 
-    train_traces = train_traces[train_traces['time:timestamp'] <= lowest_start_time]
+    highest_end_time = sorted_train['time:timestamp'].iloc[-1]
 
-    print(train_traces)
+    sorted_train = sorted_train[sorted_train['time:timestamp'] <= lowest_start_time]
+
+    sorted_test = sorted_test[sorted_test['case:REG_DATE'] >= highest_end_time]
+    
+    print(sorted_train, sorted_test)
